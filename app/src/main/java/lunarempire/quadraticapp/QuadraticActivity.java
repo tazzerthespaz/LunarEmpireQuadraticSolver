5f5b0948081ab1e9eaa0c8dc33466ef3827ddce8
Malcolm Boyd <tazzerthespaz@gmail.com>
2016-06-29 07:37:22 -0500

    Finished adding placeholders for the properly formatted math

--- app/src/main/java/lunarempire/quadraticapp/QuadraticActivity.java
+++ app/src/main/java/lunarempire/quadraticapp/QuadraticActivity.java
@@ -7,21 +7,31 @@ import android.webkit.WebView;
 import java.io.IOException;
 
 public class QuadraticActivity extends AppCompatActivity {
-    private WebView articleContent;
+    private WebView previewContent;
+    private WebView answerContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quadratic);
 
-        articleContent = (WebView) findViewById(R.id.formula_page);
-        articleContent.getSettings().setJavaScriptEnabled(true);
-        articleContent.getSettings().setBuiltInZoomControls(false);
-
+        previewContent = (WebView) findViewById(R.id.previewFormulaPage);
+        previewContent.getSettings().setJavaScriptEnabled(true);
+        previewContent.getSettings().setBuiltInZoomControls(false);
         try {
-            articleContent.loadUrl("file:///android_asset/mathFile.html");
+            previewContent.loadUrl("file:///android_asset/previewPage.html");
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        answerContent = (WebView) findViewById(R.id.rootsFormulaPage);
+        answerContent.getSettings().setJavaScriptEnabled(true);
+        answerContent.getSettings().setBuiltInZoomControls(false);
+        try {
+            answerContent.loadUrl("file:///android_asset/answerPage.html");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }

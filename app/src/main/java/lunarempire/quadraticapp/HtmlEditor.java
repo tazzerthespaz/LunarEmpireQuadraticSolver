a69b0aae564b6ab22f258894400a4d1b81be356f
Malcolm Boyd <tazzerthespaz@gmail.com>
2016-06-30 06:43:11 -0500

    Finished an untested HTMLEditor

--- app/src/main/java/lunarempire/quadraticapp/HtmlEditor.java
+++ app/src/main/java/lunarempire/quadraticapp/HtmlEditor.java
@@ -5,6 +5,9 @@ import android.content.res.AssetManager;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+
+import lunarEmpire.math.Quadratic;
 
 /**
  * Created by malcolm on 6/29/16.
@@ -14,18 +17,28 @@ public class HtmlEditor {
     Context context;
     AssetManager assetMan;
     BufferedReader br;
-    //Quadratic quad;
+
+    public Quadratic getQuad() {
+        return quad;
+    }
+
+    public void setQuad(Quadratic quad) {
+        this.quad = quad;
+    }
+
+    Quadratic quad;
 
     //TODO: Add a quadratic class here
-    public HtmlEditor(Context context, String htmlFileName) throws IOException {
-            this.context = context;
-            this.assetMan = context.getAssets();
-            openFile(htmlFileName);
+    public HtmlEditor(Context context, String htmlFileName, Quadratic quad) throws IOException {
+        this.quad = quad;
+        this.context = context;
+        this.assetMan = context.getAssets();
+        openFile(htmlFileName);
     }
 
     public void openFile(String fileName) throws IOException {
         try {
-            br = new BufferedReader(am.open(fileName));
+            br = new BufferedReader(new InputStreamReader(assetMan.open(fileName), "UTF-8"));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -33,10 +46,26 @@ public class HtmlEditor {
 
     //Takes the Quadratic class, and writes the mathematic expression for the website
     public String formatAnswer() {
+        String equationString = "$$" + quad.getRoots().getSimpOffset().getNumerator() +
+                "/" + quad.getRoots().getSimpOffset().getDenominator() +
+                "±" + quad.getRoots().getSimpOutNum().getNumerator() + "/" +
+                quad.getRoots().getSimpOutNum().getDenominator()+ "√{" +
+                quad.getRoots().getRadical().getSimpInNum() + "$$";
+        String htmlLine = "<p>" + equationString + "</p>";
+        return htmlLine;
 
     }
 
     public void update(String equation) {
-        //Write this
+        String line;
+        try {
+            while ((line = br.readLine()) != null) {
+                if (line.startsWith("<p>")) {
+                    line = formatAnswer();
+                }
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
